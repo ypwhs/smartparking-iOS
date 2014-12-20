dbd813ce3f3b332372f51090540c73043809d1c3
杨培文 <yangpeiwen@YPW-MacBook-Pro.local>
2014-12-20 16:51:54 +0800

    Initial Commit

---
+++ smartparking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  smartparking
+//
+//  Created by 杨培文 on 14/12/20.
+//  Copyright (c) 2014年 杨培文. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
